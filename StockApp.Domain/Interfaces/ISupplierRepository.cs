7e88f61038ba88261a677b3564050e93494ff4cc
EnzoGarbim <enzogarbim2004@gmail.com>
2024-06-19 20:58:25 -0300

    Criar Interface de Repositório para Supplier

---
+++ StockApp.Domain/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain.Interfaces
+{
+    public interface ISupplierRepository
+    {
+        Task<Supplier> GetByIdAsync(int id);
+        Task<IEnumerable<Supplier>> GetAllAsync();
+        Task AddAsync(Supplier supplier);
+        Task UpdateAsync(Supplier supplier);
+        Task DeleteAsync(int id);
+    }
+}
