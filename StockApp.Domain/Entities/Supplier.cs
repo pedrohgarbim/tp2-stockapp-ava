25c7ae667b8e19dac7b2869ec3e92089e1fa6867
EnzoGarbim <enzogarbim2004@gmail.com>
2024-06-19 18:47:35 -0300

    Criar Classe Supplier no Domínio - Enzo Garbim

---
+++ StockApp.Domain/Entities/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Domain
+{
+    public class Supplier
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
