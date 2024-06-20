a319d453660044d3292577c0e6e8ac8d4a8f5ce9
EnzoGarbim <enzogarbim2004@gmail.com>
2024-06-20 11:23:52 -0300

    Implementar Função de Reposição Automática de Estoque

---
+++ StockApp.API/Controllers/InventoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Domain.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    [Route("api / [controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryService _inventoryService;
+
+        public InventoryController(IInventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+
+        [HttpPost("replenish")]
+        public async Task<IActionResult> ReplenishStock()
+        {
+            await _inventoryService.ReplenishStockAsync();
+            return Ok();
+        }
+    }
+}
