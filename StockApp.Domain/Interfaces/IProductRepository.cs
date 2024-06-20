0b1e381e6386dc8b88d1d7ba46b84dc2ab0de8ff
pedrohgarbim <phg.garbim@gmail.com>
2024-06-20 13:35:11 -0300

    (13 pontos) Implementação dos Endpoints de Deleção (Delete) e Configuração no Program.cs

--- StockApp.Domain/Interfaces/IProductRepository.cs
+++ StockApp.Domain/Interfaces/IProductRepository.cs
@@ -9,7 +9,7 @@ namespace StockApp.Domain.Interfaces
         Task<Product> GetByIdAsync(int? id);
         Task<Product> AddAsync(Product product);
         Task<Product> UpdateAsync(Product product);
-        Task<Product> Remove(Product product);
+        Task DeleteAsync(int? id);
         Task<IEnumerable<Product>> GetLowStockAsync(int threshold);
         Task BulkUpdateAsync(IEnumerable<Product> products);
     }
