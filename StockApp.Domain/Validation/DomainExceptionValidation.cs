36988dd5b7c346cd86e5ecf86d18e3aadf9df60a
joaoveasey <jovepedro@gmail.com>
2024-06-21 11:06:17 -0300

    14. (5 pontos) Criação de Exceções Personalizadas para Autenticação

--- StockApp.Domain/Validation/DomainExceptionValidation.cs
+++ StockApp.Domain/Validation/DomainExceptionValidation.cs
@@ -9,12 +9,28 @@ namespace StockApp.Domain.Validation
     public class DomainExceptionValidation : Exception
     {
         public DomainExceptionValidation(string error) : base(error)
-        {}
+        { }
 
         public static void When(bool hasError, string error)
         {
-            if(hasError)
+            if (hasError)
                 throw new DomainExceptionValidation(error);
+        }
+
+        public class AuthenticationException : Exception
+        {
+            public AuthenticationException(string message) : base(message)
+            {
+
+            }
+        }
+
+        public class AuthorizationException : Exception
+        {
+            public AuthorizationException(string message) : base(message)
+            {
+
+            }
         }
     }
 }
