8cd7b0f2901f02964a3411e96c5e22a60cb05b51
joaoveasey <jovepedro@gmail.com>
2024-06-21 17:31:58 -0300

    29. (13 pontos) Implementar Autenticação Multi-Fator (MFA)

---
+++ StockApp.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("mfa auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMfaService _mfaService;
+
+        public AuthController(IMfaService mfaService)
+        {
+            _mfaService = mfaService;
+        }
+
+        [HttpPost("generate-otp")]
+        public ActionResult<string> GenerateOtp()
+        {
+            var otp = _mfaService.GenerateOtp();
+            return Ok(otp);
+        }
+
+        [HttpPost("validate-otp")]
+        public ActionResult<bool> ValidateOtp([FromBody] string userOtp)
+        {
+            string storedOtp;
+            var isValid = _mfaService.ValidateOtp(userOtp, out storedOtp);
+            return Ok(isValid);
+        }
+    }
+}
---
+++ StockApp.Application/Services/MfaService.cs
@@ -0,0 +1,46 @@
+﻿using StockApp.Application.DTOs;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    public class MfaService : IMfaService
+    {
+        private readonly ConcurrentDictionary<string,string> otpCache = new ConcurrentDictionary<string,string>();
+
+        public string GenerateOtp()
+        {
+            var otp = new Random().Next(100000, 999999).ToString();
+            return otp;
+        }
+
+        public bool ValidateOtp(string userOtp, out string storedOtp)
+        {
+            storedOtp = string.Empty;
+
+            foreach (var otpEntry in otpCache)
+            {
+                if(otpEntry.Value == userOtp)
+                {
+                    storedOtp = otpEntry.Value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void StoredOtp(string key, string otp)
+        {
+            otpCache[key] = otp;
+        }
+
+        public void RemoveOtp(string key)
+        {
+            otpCache.TryRemove(key, out _);
+        }
+    }
+}
