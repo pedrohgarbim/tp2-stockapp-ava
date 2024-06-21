c99705ddbe18e924abed61211f4e068bc25adc5c
pedrohgarbim <phg.garbim@gmail.com>
2024-06-21 18:15:17 -0300

    (13 pontos) Criar Dashboard de Vendas

--- StockApp.Domain/Entities/Order.cs
+++ StockApp.Domain/Entities/Order.cs
@@ -9,6 +9,10 @@ namespace StockApp.Domain.Entities
     public class Order
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
         public decimal TotalAmount { get; set; }
     }
 }
