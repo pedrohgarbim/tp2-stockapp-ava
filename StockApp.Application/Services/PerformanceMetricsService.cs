0542726ebed83f2381a86762e1ca9be84d1ab42f
NicolasCanonico0 <nicolas.undiciati@fatec.sp.gov.br>
2024-06-23 22:52:17 -0300

    Implementar Sistema de Métricas de Performance

---
+++ StockApp.Application/Services/PerformanceMetricsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Application.Interfaces;
+using StockApp.Application.DTOs;
+
+namespace StockApp.Application.Services
+{
+    public class PerformanceMetricsService : IPerformanceMetricsService
+    {
+        public async Task<PerformanceMetricsDto> GetMetricsAsync()
+        {
+            return new PerformanceMetricsDto
+            {
+                CpuUsage = 75.5,
+                MemoryUsage = 2048,
+                ResponseTime = 250
+            };
+        }
+    }
+}
