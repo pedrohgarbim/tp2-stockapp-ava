45cc6acd601f8034361778e8fe36565d5fa5d418
NicolasCanonico0 <nicolas.undiciati@fatec.sp.gov.br>
2024-06-23 23:28:13 -0300

    Implementar Sistema de Gestão de Projetos

---
+++ StockApp.Application/Services/ProjectManagementService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Application.DTOs.Projects;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.Application.Services
+{
+    public class ProjectManagementService : IProjectManagementService
+    {
+        public async Task<ProjectDto> CreateProjectAsync(CreateProjectDto createProjectDto)
+        {
+            return new ProjectDto
+            {
+                Id = 1,
+                Name = createProjectDto.Name,
+                Description = createProjectDto.Description,
+                StartDate = createProjectDto.StartDate,
+                EndDate = createProjectDto.EndDate,
+            };
+        }
+    }
+}
