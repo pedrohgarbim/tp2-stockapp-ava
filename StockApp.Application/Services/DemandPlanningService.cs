c900bd97a4c66226f9053fefe231f6a662aaca82
NicolasCanonico0 <nicolas.undiciati@fatec.sp.gov.br>
2024-06-23 23:55:06 -0300

    Implementar Sistema de Planejamento de Demanda

---
+++ StockApp.Application/Services/DemandPlanningService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.Application.Services
+{
+    public class DemandPlanningService : IDemandPlanningService
+    {
+        public async Task<DemandForecastDto> ForecastDemandAsync()
+        {
+            return new DemandForecastDto
+            {
+                ProductId = 1,
+                ForecastDemand = 100
+            };
+        }
+    }
+}
